984c8b3076dd990e83f70cf3113a88a20b5dd6c8
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2012-12-16 15:19:57 -0500

    Clean-up source style using astyle [WIP]

--- include/webvtt/parser.h
+++ include/webvtt/parser.h
@@ -1,29 +1,29 @@
 #ifndef __WEBVTT_PARSER_H__
-#	define __WEBVTT_PARSER_H__
-#	include "string.h"
-#	include "cue.h"
-#	include "error.h"
+# define __WEBVTT_PARSER_H__
+# include "string.h"
+# include "cue.h"
+# include "error.h"
 
 #if defined(__cplusplus) || defined(c_plusplus)
 extern "C" {
 #endif
 
-typedef struct webvtt_parser_t *webvtt_parser;
-typedef struct webvtt_struct_params_t webvtt_struct_params;
+  typedef struct webvtt_parser_t *webvtt_parser;
+  typedef struct webvtt_struct_params_t webvtt_struct_params;
 
-/**
- * Allows application to request error reporting
- */
-typedef int (WEBVTT_CALLBACK *webvtt_error_fn_ptr)( void *userdata, webvtt_uint line, webvtt_uint col, 
-	webvtt_error error );
-typedef void (WEBVTT_CALLBACK *webvtt_cue_fn_ptr)( void *userdata, webvtt_cue cue );
+  /**
+   * Allows application to request error reporting
+   */
+  typedef int ( WEBVTT_CALLBACK *webvtt_error_fn_ptr )( void *userdata, webvtt_uint line, webvtt_uint col,
+      webvtt_error error );
+  typedef void ( WEBVTT_CALLBACK *webvtt_cue_fn_ptr )( void *userdata, webvtt_cue cue );
 
-	
-WEBVTT_EXPORT webvtt_status webvtt_create_parser( webvtt_cue_fn_ptr on_read, webvtt_error_fn_ptr on_error, void *
-									userdata, webvtt_parser *ppout );
-WEBVTT_EXPORT void webvtt_delete_parser( webvtt_parser parser );
-WEBVTT_EXPORT webvtt_status webvtt_parse_chunk( webvtt_parser self, const void *buffer, webvtt_uint len, webvtt_bool is_final_chunk );
-WEBVTT_EXPORT webvtt_status webvtt_parse_cuetext( webvtt_cue cue );
+
+  WEBVTT_EXPORT webvtt_status webvtt_create_parser( webvtt_cue_fn_ptr on_read, webvtt_error_fn_ptr on_error, void *
+      userdata, webvtt_parser *ppout );
+  WEBVTT_EXPORT void webvtt_delete_parser( webvtt_parser parser );
+  WEBVTT_EXPORT webvtt_status webvtt_parse_chunk( webvtt_parser self, const void *buffer, webvtt_uint len, webvtt_bool is_final_chunk );
+  WEBVTT_EXPORT webvtt_status webvtt_parse_cuetext( webvtt_cue cue );
 
 #if defined(__cplusplus) || defined(c_plusplus)
 }
