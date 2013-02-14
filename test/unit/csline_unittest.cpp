4c6f4cb32f4c86f3352dd0b0d13768ef667d9f57
Caitlin Potter <snowball@defpixel.com>
2013-02-14 10:03:27 -0500

    Fixes #103 - Fix assertion, wrap lines, add safety assertion
    - Assert errorCount() == 1 before asserting error conditions, avoid
      bad access
    - Assert snapToLines == true rather than false when it's expected to
      be the default setting, as per the rules of the spec.
    - Use convenience error condition assertion function
    - Wrap lines to 80 cols

--- test/unit/csline_unittest.cpp
+++ test/unit/csline_unittest.cpp
@@ -765,25 +765,27 @@ TEST_F(CueSettingLine, UppercaseKeyword)
  *
  * http://dev.w3.org/html5/webvtt/#parse-the-webvtt-settings (11/28/2012):
  * If name is a case-sensitive match for "line"
- *  7. If the last character in value is a U+0025 PERCENT SIGN character (%), but number is not in the range 0 = number = 100, then jump to the step labeled next setting.
+ *  7. If the last character in value is a U+0025 PERCENT SIGN character (%),
+ *     but number is not in the range 0 = number = 100, then jump to the step
+ *     labeled next setting.
  */
 TEST_F(CueSettingLine, PercentNegative)
 {
   loadVtt( "cue-settings/line/bad-pct-negative.vtt", 1 );
-  const Error &err = getError( 0 );
+  ASSERT_EQ( 1, errorCount() );
+  
   /**
-   * Line should be "auto" and snap-to-lines should be true
-     * because the malformed settinga should be skipped
-     * and "auto" and true are default.
+   * Line should be "auto" and snap-to-lines should be true because the
+   * malformed settinga should be skipped and "auto" and true are default.
    */
   ASSERT_TRUE( getCue( 0 ).isLinePositionAuto() );
-  ASSERT_FALSE( getCue( 0 ).snapToLines() );
+  ASSERT_TRUE( getCue( 0 ).snapToLines() );
+
   /**
-   * We're expecting a WEBVTT_UNEXPECTED_WHITESPACE error on the 30th column of the 3rd line
+   * We're expecting a WEBVTT_UNEXPECTED_WHITESPACE error on the 30th column of
+   * the 3rd line
    */
-  ASSERT_EQ( WEBVTT_UNEXPECTED_WHITESPACE, err.error() );
-  ASSERT_EQ( 3, err.line() );
-  ASSERT_EQ( 30, err.column() );
+  assertEquals( getError( 0 ), WEBVTT_UNEXPECTED_WHITESPACE, 3, 30 );
 }
 
 /**
