afa5a16adc8f5d99e86b5690f28cd67436769259
Caitlin Potter <snowball@defpixel.com>
2013-02-14 11:53:36 -0500

    Fixes #113 - Correctly assert snapToLines == TRUE, wrap whitespace
    - ASSERT_FALSE -> ASSERT_TRUE for snapToLines()
    - assert errorCount == 1 before checking error conditions
    - use error condition assertEquals helper to shrink code
    - wrap lines to 80 columns

--- test/unit/csline_unittest.cpp
+++ test/unit/csline_unittest.cpp
@@ -522,26 +522,28 @@ TEST_F(CueSettingLine, NoKeyword)
  * Test that the parser requires a colon.
  *
  * http://dev.w3.org/html5/webvtt/#parse-the-webvtt-settings (11/27/2012):
- * 1. If setting does not contain a U+003A COLON character (:), or if the first U+003A COLON character (:) in setting is either the first or last character of setting, then jump to the step labeled next setting.
- * 5. Next setting: Continue to the next token, if any.
+ * 1. If setting does not contain a U+003A COLON character (:), or if the first
+ *    U+003A COLON character (:) in setting is either the first or last character
+ *    of setting, then jump to the step labeled next setting.  5. Next setting:
+ *    Continue to the next token, if any.
  */
 TEST_F(CueSettingLine, NoDelimiter)
 {
   loadVtt( "cue-settings/line/no-delimiter.vtt", 1 );
-  const Error &err = getError( 0 );
+  ASSERT_EQ( 1, errorCount() );
+
   /**
-   * Line should be "auto" and snap-to-lines should be true
-     * because the malformed setting should be skipped
-     * and "auto" and true are default.
+   * Line should be "auto" and snap-to-lines should be true because the
+   * malformed setting should be skipped and "auto" and true are default.
    */
   ASSERT_TRUE( getCue( 0 ).isLinePositionAuto() );
-  ASSERT_FALSE( getCue( 0 ).snapToLines() );
+  ASSERT_TRUE( getCue( 0 ).snapToLines() );
+  
   /**
-   * We're expecting a WEBVTT_INVALID_CUESETTING error on the 25th column of the 3rd line
+   * We're expecting a WEBVTT_INVALID_CUESETTING error on the 25th column of the
+   * 3rd line
    */
-  ASSERT_EQ( WEBVTT_INVALID_CUESETTING, err.error() );
-  ASSERT_EQ( 3, err.line() );
-  ASSERT_EQ( 25, err.column() );
+  assertEquals( getError( 0 ), WEBVTT_INVALID_CUESETTING, 3, 25 );
 }
 
 /**
