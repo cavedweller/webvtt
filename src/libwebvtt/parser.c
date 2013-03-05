2f00ead8819ada2b9f101b51c883e8455cc9c80a
Ralph Giles <giles@mozilla.com>
2013-03-05 15:09:38 -0800

    Convert plformat segfaults into test failures.
    This doesn't fix the logic errors in the parser, it just adds
    guards to return early instead of segfaulting. This lets us
    continue to the next unit test and get better coverage.

--- src/libwebvtt/parser.c
+++ src/libwebvtt/parser.c
@@ -591,7 +591,7 @@ _recheck:
               ( BAD_TIMESTAMP( cue->from )
                 ? WEBVTT_EXPECTED_TIMESTAMP
                 : WEBVTT_MALFORMED_TIMESTAMP ), last_column  );
-            if( !webvtt_isdigit( self->token[self->token_pos - 1] ) ) {
+            if( self->token_pos && !webvtt_isdigit( self->token[self->token_pos - 1] ) ) {
               while( pos < len && buffer[pos] != 0x09 && buffer[pos] != 0x20 ) { ++pos; }
             }
             if( BAD_TIMESTAMP( cue->from ) )
@@ -1219,6 +1219,13 @@ _recheck:
           webvtt_state *st = FRAMEUP( 1 );
           webvtt_string text = st->v.text;
 
+          /* FIXME: guard inconsistent state */
+          if (!cue) {
+            ERROR( WEBVTT_PARSE_ERROR );
+            status = WEBVTT_PARSE_ERROR;
+            goto _finish;
+          }
+
           st->type = V_NONE;
           st->v.cue = NULL;
 
@@ -1252,7 +1259,7 @@ _recheck:
             }
           } else {
             /* It is a cue-id */
-            if( cue->flags & CUE_HAVE_ID ) {
+            if( cue && cue->flags & CUE_HAVE_ID ) {
               /**
                * This isn't actually a cue-id, because we already
                * have one. It seems to be cuetext, which is occurring
