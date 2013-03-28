b2e7a4b7a5386f37b73c6d964bacd7f7e90e2ea6
Shayan Zafar Ahmad <Shayan@Shayan-Zafar-Ahmads-MacBook-Pro.local>
2013-03-28 03:20:21 -0400

    fixed filenames for plescapecharacter unit tests

--- test/unit/plescapecharacter_unittest.cpp
+++ test/unit/plescapecharacter_unittest.cpp
@@ -965,7 +965,7 @@ TEST_F(PayloadEscapeCharacter, AmpersandWithinTag)
 */
 TEST_F(PayloadEscapeCharacter, AmpersandWithinMultipleTags)
 {
-  loadVtt( "payload/escape-character/i-tag-within-b-tag-with-ampersand.vtt", 1 );
+  loadVtt( "payload/escape-character/i-within-b-tag-with-ampersand.vtt", 1 );
 
   /* verify bold tag */
   const Node boldTag = getHeadOfCue(0);
@@ -993,7 +993,7 @@ TEST_F(PayloadEscapeCharacter, AmpersandWithinMultipleTags)
 */
 TEST_F(PayloadEscapeCharacter, AmpersandOutsideTwoTags)
 {
-  loadVtt( "payload/escape-character/i-tag-within-b-tag-with-ampersand-outside.vtt", 1 );
+  loadVtt( "payload/escape-character/i-within-b-tag-with-ampersand-outside.vtt", 1 );
 
   /* verify bold tag */
   const Node boldTag = getHeadOfCue(0);
@@ -1051,7 +1051,7 @@ TEST_F(PayloadEscapeCharacter, AmpersandOnNewLineAfterTwoTagsWithClass)
 */
 TEST_F(PayloadEscapeCharacter, AmpersandInsideOneTagWithClass)
 {
-  loadVtt( "payload/escape-character/ampersand-within-tag-with-class.vtt", 1 );
+  loadVtt( "payload/escape-character/ampersand-inside-tag-with-class.vtt", 1 );
 
   /* verify italic tag */
   const Node italicTag = getHeadOfCue(0);
