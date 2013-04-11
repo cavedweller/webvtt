4e28dfc78dc37a088a55878e05e5c9889371e876
Caitlin Potter <snowball@defpixel.com>
2013-04-11 02:05:17 -0700

    Fix cigeneral_unittest
    Removed C comments from head of test/unit/cue-ids/basic_pass.vtt
    Wrapped lines in cigeneral_unittest.cpp to 80 columns
    Make use of helper functions for error checks where possible
    Expect rather than assert where possible
    Safety checks to prevent buffer overflows/out of bounds access

--- test/unit/cigeneral_unittest.cpp
+++ test/unit/cigeneral_unittest.cpp
@@ -5,17 +5,19 @@ class CueIdGeneral : public CueTest { };
  * Test to determine if the most basic one-character cue id passes
  *
  * From http://dev.w3.org/html5/webvtt/#webvtt-cue-identifier (11/24/2012)
- * A WebVTT cue identifier is any sequence of one or more characters not containing the substring "-->"
- * (U+002D HYPHEN-MINUS, U+002D HYPHEN-MINUS, U+003E GREATER-THAN SIGN), nor containing any U+000A LINE FEED (LF)
+ *
+ * A WebVTT cue identifier is any sequence of one or more characters not
+ * containing the substring "-->" (U+002D HYPHEN-MINUS, U+002D HYPHEN-MINUS,
+ * U+003E GREATER-THAN SIGN), nor containing any U+000A LINE FEED (LF)
  * characters or U+000D CARRIAGE RETURN (CR) characters.
  *
- * Note: A WebVTT cue identifier can be used to reference a specific cue, for example from script or CSS.
+ * Note: A WebVTT cue identifier can be used to reference a specific cue, for
+ * example from script or CSS.
  */
-TEST_F(CueIdGeneral, BasicPass) //C++ exception: "std::bad_alloc"
+TEST_F(CueIdGeneral, BasicPass)
 {
-  loadVtt( "cue-ids/basic_pass.vtt" );
-
-  ASSERT_EQ( 0, errorCount() ) << "This file should contain no errors.";
+  loadVtt( "cue-ids/basic_pass.vtt", 1 );
+  EXPECT_EQ( 0, errorCount() ) << "This file should contain no errors.";
 }
 
 /**
@@ -23,37 +25,40 @@ TEST_F(CueIdGeneral, BasicPass) //C++ exception: "std::bad_alloc"
  * This test floods the cue ID with characters, but should pass
  *
  * From http://dev.w3.org/html5/webvtt/#webvtt-cue-identifier (11/24/2012)
- * A WebVTT cue identifier is any sequence of one or more characters not containing the substring "-->"
- * (U+002D HYPHEN-MINUS, U+002D HYPHEN-MINUS, U+003E GREATER-THAN SIGN), nor containing any U+000A LINE FEED (LF)
+ *
+ * A WebVTT cue identifier is any sequence of one or more characters not
+ * containing the substring "-->" (U+002D HYPHEN-MINUS, U+002D HYPHEN-MINUS,
+ * U+003E GREATER-THAN SIGN), nor containing any U+000A LINE FEED (LF)
  * characters or U+000D CARRIAGE RETURN (CR) characters.
  *
- * Note: A WebVTT cue identifier can be used to reference a specific cue, for example from script or CSS.
+ * Note: A WebVTT cue identifier can be used to reference a specific cue, for
+ * example from script or CSS.
  */
-TEST_F(CueIdGeneral, LongString) //Test currently throws a webvtt cue incomplete error, however it should pass
+TEST_F(CueIdGeneral, LongString)
 {
-  loadVtt( "cue-ids/long_string.vtt" );
-  const Error &err = getError( 0 );
-  //ASSERT_EQ( WEBVTT_CUE_INCOMPLETE, err.error() );
-
-  ASSERT_EQ( 0, errorCount() ) << "This file should contain no errors.";
+  loadVtt( "cue-ids/long_string.vtt", 1 );
+  EXPECT_EQ( 0, errorCount() ) << "This file should contain no errors.";
 }
 
 /**
- * Test if parser fails if the webvtt file only has a cue id (no timestamp or payload)
+ * Test if parser fails if the webvtt file only has a cue id (no timestamp or
+ * payload)
  *
  * From http://dev.w3.org/html5/webvtt/#webvtt-cue-identifier (11/24/2012)
- * A WebVTT cue identifier is any sequence of one or more characters not containing the substring "-->"
- * (U+002D HYPHEN-MINUS, U+002D HYPHEN-MINUS, U+003E GREATER-THAN SIGN), nor containing any U+000A LINE FEED (LF)
+ *
+ * A WebVTT cue identifier is any sequence of one or more characters not
+ * containing the substring "-->" (U+002D HYPHEN-MINUS, U+002D HYPHEN-MINUS,
+ * U+003E GREATER-THAN SIGN), nor containing any U+000A LINE FEED (LF)
  * characters or U+000D CARRIAGE RETURN (CR) characters.
  *
- * Note: A WebVTT cue identifier can be used to reference a specific cue, for example from script or CSS.
+ * Note: A WebVTT cue identifier can be used to reference a specific cue, for
+ * example from script or CSS.
  */
 TEST_F(CueIdGeneral, IdOnly)
 {
-  loadVtt( "cue-ids/id_only.vtt" );
-  const Error &err = getError( 0 );
+  loadVtt( "cue-ids/id_only.vtt", 0 );
+  ASSERT_LE( 1, errorCount() ) << "This file should contain at least 1 error";
+  EXPECT_EQ( 1, errorCount() ) << "This file should contain 1 error";
 
-  ASSERT_EQ( WEBVTT_CUE_INCOMPLETE, err.error() );
-  ASSERT_EQ( 4, err.line() );
-  ASSERT_EQ( 0, err.column() );
+  expectEquals( getError( 0 ), WEBVTT_CUE_INCOMPLETE, 3, 2 );
 }
