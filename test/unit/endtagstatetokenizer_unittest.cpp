f14972a25c75a1a721f49c67ccadb63aedfc79a6
Rick Eyre <rick.eyre@hotmail.com>
2013-04-15 20:48:37 -0400

    Add end tag state tokenizer tests.

---
+++ test/unit/endtagstatetokenizer_unittest.cpp
@@ -0,0 +1,49 @@
+#include "cuetexttokenizer_fixture"
+
+class EndTagStateTokenizerTest : public CueTextTokenizerTest
+{
+  public:
+    void EndTagTokenize( const char *text ) {
+      state = END_TAG;
+      pos = reinterpret_cast<const webvtt_byte *>( text );
+      status = webvtt_end_tag_state( &pos, &state, &res );
+    }
+};
+
+/*
+ * Tests if the end tag state tokenizer parses simple text.
+ */
+TEST_F(EndTagStateTokenizerTest, BasicText)
+{
+  EndTagTokenize( "ruby" );
+  EXPECT_EQ( WEBVTT_SUCCESS, GetStatus() );
+  EXPECT_EQ( '\0', GetCurrentChar() );
+  EXPECT_EQ( END_TAG, GetState() );
+  EXPECT_STREQ( "ruby", ParsedText() );
+}
+
+/*
+ * Tests if the end tag state tokenizer exits correctly upon encountering an
+ * '>' character.
+ */
+TEST_F(EndTagStateTokenizerTest, GTFinished)
+{
+  EndTagTokenize( "ruby>Text" );
+  EXPECT_EQ( WEBVTT_SUCCESS, GetStatus() );
+  EXPECT_EQ( '>', GetCurrentChar() );
+  EXPECT_EQ( END_TAG, GetState() );
+  EXPECT_STREQ( "ruby", ParsedText() );
+}
+
+/*
+ * Tests if the end tag state tokenizer exits correctly upon encountering an
+ * '\0' character.
+ */
+TEST_F(EndTagStateTokenizerTest, NullByteFinished)
+{
+  EndTagTokenize( "ruby\0" );
+  EXPECT_EQ( WEBVTT_SUCCESS, GetStatus() );
+  EXPECT_EQ( '\0', GetCurrentChar() );
+  EXPECT_EQ( END_TAG, GetState() );
+  EXPECT_STREQ( "ruby", ParsedText() );
+}
