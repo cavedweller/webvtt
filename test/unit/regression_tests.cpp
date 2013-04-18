172b51ee28fdd3590f65a7ef86eb766c8846db56
Caitlin Potter <snowball@defpixel.com>
2013-04-18 10:26:58 -0400

    Add regression test for bug 853589 testcase 1

--- test/unit/regression_tests.cpp
+++ test/unit/regression_tests.cpp
@@ -11,3 +11,11 @@ TEST_F(RegressionTest,Bug853879)
   EXPECT_TRUE( parse( "853879-1.vtt" ) );
 }
 
+// Bug853589 - Crash [@parse_cueparams]
+//
+// File contains syntax errors (bad end timestamp), but should parse
+// successfully due to not aborting on errors.
+TEST_F(RegressionTest,Bug853589)
+{
+  EXPECT_TRUE( parse( "853589-1.vtt" ) );
+}
