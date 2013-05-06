bb41b6128f388d2f0f2798dc7af3bb64801a3b92
Caitlin Potter <snowball@defpixel.com>
2013-05-06 09:47:55 -0700

    Don't generate/include config.h
    Remove webvtt-config.h from libwebvtt(xx)_SOURCES

--- include/webvtt/util.h
+++ include/webvtt/util.h
@@ -32,17 +32,28 @@
 extern "C" {
 #endif
 
-# if defined(_WIN32) || defined(__WIN32__) || defined(__TOS_WIN__)
-#    || defined(__WINDOWS__)
-#   if !WEBVTT_NO_CONFIG_H
-#     include "webvtt-config-win32.h"
+# ifdef HAVE_CONFIG_H
+/**
+ * Maybe this is a bad idea!
+ *
+ * But realistically, it's not likely to hurt anything.
+ */
+#   include "config.h"
+# endif
+
+# if defined(HAVE_STDINT_H)
+/**
+ * If config.h says we have stdint.h, then trust it.
+ */
+#   include <stdint.h>
+# else
+/**
+ * Assuming that libc provides stdint.h unless we have a good reason to believe
+ * it doesn't... MSVC does not ship stdint.h prior to MSVC2010.
+ */
+#   if !defined(_MSC_VER) || _MSC_VER >= 1600
+#     include <stdint.h>
 #   endif
-#   define WEBVTT_OS_WIN32 1
-#   if defined(_WIN64)
-#     define WEBVTT_OS_WIN64 1
-#   endif
-# elif !WEBVTT_NO_CONFIG_H
-#   include <webvtt/webvtt-config.h>
 # endif
 
 # if defined(_MSC_VER)
